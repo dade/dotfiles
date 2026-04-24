db15b0000a03d44ddd21e81139f0564083820471
Dale Emasiri <demasiri@gmail.com>
2026-04-24 11:39:42 +0200

    adding description

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -4,6 +4,10 @@ return {
     -- your options
   },
   keys = { -- load the plugin only when using it's keybinding:
-    { "<leader><S-u>", "<cmd>lua require('undotree').toggle()<cr>" },
+    {
+      "<leader><S-u>",
+      "<cmd>lua require('undotree').toggle()<cr>",
+      desc = "Undotree",
+    },
   },
 }
